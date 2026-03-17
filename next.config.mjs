91b1216f1a5865d0160f3c23f0bab1ff33b5bc89
Annlyn <annlynanto@gmail.com>
2026-03-17 13:19:58 +0530

    pin jsdom fix ESM error on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["jsdom", "@mozilla/readability", "canvas"],
+  experimental: {
+    turbo: {
+      resolveExtensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+  },
 };
 
 export default nextConfig;

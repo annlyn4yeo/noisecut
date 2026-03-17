2367ab1f67b1954de819a0787cc69a779f6f2e07
Annlyn <annlynanto@gmail.com>
2026-03-17 13:00:27 +0530

    load libs externally - build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: ["jsdom", "@mozilla/readability", "canvas"],
+};
 
 export default nextConfig;
